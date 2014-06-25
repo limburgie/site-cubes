d34c71f88da40de683687eed12a4d183db4309e1
Peter Mesotten <peter.mesotten@aca-it.be>
2014-06-25 11:03:52 +0200

    Added repository caching

--- src/main/java/be/webfactor/sitecubes/repository/PageRepository.java
+++ src/main/java/be/webfactor/sitecubes/repository/PageRepository.java
@@ -5,11 +5,19 @@ import be.webfactor.sitecubes.domain.PageLayout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.QueryHint;
 import java.util.List;
 
 public interface PageRepository extends JpaRepository<Page, Long> {
+
+	@QueryHints({@QueryHint(name = "org.hibernate.cacheable", value = "true")})
+	Page findOne(Long aLong);
+
+	@QueryHints({@QueryHint(name = "org.hibernate.cacheable", value = "true")})
+	List<Page> findAll();
 
 	@Query("FROM Page WHERE parent IS NULL")
 	List<Page> getRootPages();
@@ -17,6 +25,8 @@ public interface PageRepository extends JpaRepository<Page, Long> {
 	@Query("FROM Page WHERE friendlyUrl=:friendlyUrl")
 	Page findByFriendlyUrl(@Param("friendlyUrl") String friendlyUrl);
 
-	@Modifying @Query("UPDATE Page SET layout=?2 WHERE layout=?1")
+	@Modifying
+	@Query("UPDATE Page SET layout=?2 WHERE layout=?1")
 	void updatePageLayout(PageLayout layout, PageLayout defaultLayout);
+
 }
