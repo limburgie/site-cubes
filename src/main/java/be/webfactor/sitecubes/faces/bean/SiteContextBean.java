8b0401168513bde8a931663eda7fbf59d066abeb
Peter Mesotten <peter.mesotten@aca-it.be>
2014-11-15 14:08:51 +0100

    #31: Added utility method in siteContextBean for easy retrieval of site content by title

--- src/main/java/be/webfactor/sitecubes/faces/bean/SiteContextBean.java
+++ src/main/java/be/webfactor/sitecubes/faces/bean/SiteContextBean.java
@@ -1,7 +1,9 @@
 package be.webfactor.sitecubes.faces.bean;
 
+import be.webfactor.sitecubes.domain.ContentItem;
 import be.webfactor.sitecubes.domain.Site;
 import be.webfactor.sitecubes.faces.helper.FacesUtil;
+import be.webfactor.sitecubes.service.ContentItemService;
 import be.webfactor.sitecubes.service.SiteService;
 import org.springframework.context.annotation.Scope;
 
@@ -16,6 +18,7 @@ import java.util.List;
 public class SiteContextBean implements Serializable {
 
 	@Inject private SiteService siteService;
+	@Inject private ContentItemService contentItemService;
 	@Inject private FacesUtil facesUtil;
 
 	private Site site;
@@ -25,6 +28,11 @@ public class SiteContextBean implements Serializable {
 	public void init() {
 		initSite();
 		initOtherSites();
+	}
+
+	public String getContentByTitle(String title) {
+		ContentItem item = contentItemService.getItemByTitle(site, title);
+		return item == null ? "" : item.getContent();
 	}
 
 	private void initOtherSites() {
