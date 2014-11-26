213efe8c44fa4e03948e075544d5b29a9f744cad
Peter Mesotten <peter.mesotten@aca-it.be>
2014-11-26 14:09:04 +0100

    #7: Added first name and last name

--- src/main/java/be/webfactor/sitecubes/domain/User.java
+++ src/main/java/be/webfactor/sitecubes/domain/User.java
@@ -6,6 +6,12 @@ import javax.persistence.Entity;
 @Entity
 public class User extends BaseEntity {
 
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+
 	@Column(name = "username", unique = true, nullable = false)
 	private String username;
 
@@ -18,6 +24,22 @@ public class User extends BaseEntity {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public String getPassword() {
