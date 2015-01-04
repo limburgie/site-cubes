158ff90be8d8f17a2160633347fbdcca98ca4f9c
Peter Mesotten <peter.mesotten@aca-it.be>
2015-01-04 16:41:42 +0100

    Remove @PreAuthorizes

--- src/main/java/be/webfactor/sitecubes/service/ContentItemService.java
+++ src/main/java/be/webfactor/sitecubes/service/ContentItemService.java
@@ -2,7 +2,6 @@ package be.webfactor.sitecubes.service;
 
 import be.webfactor.sitecubes.domain.ContentItem;
 import be.webfactor.sitecubes.domain.Site;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
@@ -12,13 +11,10 @@ public interface ContentItemService {
 
 	List<ContentItem> getItems(Site site);
 
-	@PreAuthorize("hasPermission(#item.site, 'admin')")
 	ContentItem save(ContentItem item);
 
-	@PreAuthorize("hasPermission(#item.site, 'admin')")
 	void delete(ContentItem item);
 
-	@PreAuthorize("hasPermission(#site, 'admin')")
 	void deleteSiteContent(Site site);
 
 	ContentItem getItem(long itemId);
